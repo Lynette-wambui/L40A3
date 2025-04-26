cfc7c8dc8c8e76e7b9fe74d409e7fa981a9cf7b9
LYNETTE WAMBUI <lynettemuraga47@gmail.com>
2025-04-26 18:51:14 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,16 @@
+first_name = "Chaebol "
+last_name = " Enterprises"
+full_name = first_name+last_name
+
+word = 'HELLO!'*5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String Multiplied 5 times gives this result :", word)
+
+word = 'Management'
+print("Length of String :", len(word))
+print("First Letter of String :", word[0])
+print("Last Letter of String :", word[5])
+print("String Sliced :", word[0:4])
